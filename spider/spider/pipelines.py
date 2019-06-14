831726cccf5b259b0e71823f61538b0ad8c1de46
chunmin.lcm <chunmin.lcm@alibaba-inc.com>
2019-06-14 22:57:21 +0800

    [dev]

---
+++ spider/spider/pipelines.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import SpidersKeList from .items
+
+class SpiderPipeline(object):
+
+    username_str = 'breadt'
+    password_str = 'Breadt@2019'
+
+    def connect(self):
+
+        username = urllib.parse.quote_plus(self.username_str)
+        password = urllib.parse.quote_plus(self.password_str)
+
+        self.client = pymongo.MongoClient('mongodb://%s:%s@10.12.86.109:27017/' % (username, password))
+        self.db = self.client["ke"]
+
+    def open_spider(self, spider):
+        print('SpidersPipeline.open_spider')
+        self.connect()
+
+    def process_item(self, item, spider):
+
+        if isinstance(item, SpidersKeList):
+            feedback = self.db["kelist"].insert_one({'content': item['content'], 'date': item['date']})
+
+        return item
+
+    def close_spider(self, spider):
+        self.client.close()
---
+++ spider/spider/spiders/ke.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import SpidersKeList from ..items
+
+
+class KeSpider(scrapy.Spider):
+    name = 'ke'
+    allowed_domains = ['ke.com']
+    start_urls = ['http://ke.com/']
+
+    domain = 'https://sh.ke.com/ershoufang/zhabei/pg%dl2/'
+
+    def start_requests(self):
+        # self.connect()
+
+        current_date = datetime.datetime.now()
+
+        for i in range(1, 45):
+            yield scrapy.Request(
+                method='get',
+                url=self.domain % (i),
+                callback=self.parse,
+                meta={
+                    'date': current_date.strftime("%Y-%m-%d")
+                }
+            )
+
+    def parse(self, response):
+        if response.text is not None:
+            yield SpidersKeList(content=response.text, date=response.meta['date'])
